ef580baec0e0f48fe04f5bc624f04d67a8872d59
Tobias Olsson <tobias.olsson@youcruit.com>
2015-10-15 11:11:05 +0200

    More tests (this time on search)

--- src/test/java/com/youcruit/billogram/BillogramIT.java
+++ src/test/java/com/youcruit/billogram/BillogramIT.java
@@ -1,8 +1,12 @@
 package com.youcruit.billogram;
 
+import static com.youcruit.billogram.client.BillogramClient.SendCommand.SEND;
+import static com.youcruit.billogram.objects.request.billogram.BillogramOrderField.CUSTOMER_NO;
+import static com.youcruit.billogram.objects.request.billogram.SendMethod.EMAIL;
 import static com.youcruit.billogram.objects.response.error.BillogramErrors.NOT_AVAILABLE_YET;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -14,10 +18,17 @@ import com.youcruit.billogram.client.BillogramClient;
 import com.youcruit.billogram.client.CustomerClient;
 import com.youcruit.billogram.client.http.HttpClient;
 import com.youcruit.billogram.exception.ApiException;
+import com.youcruit.billogram.objects.request.FilterType;
+import com.youcruit.billogram.objects.request.Search;
+import com.youcruit.billogram.objects.request.SortDirection;
+import com.youcruit.billogram.objects.request.billogram.BillogramFilterField;
+import com.youcruit.billogram.objects.request.billogram.BillogramOrderField;
+import com.youcruit.billogram.objects.request.billogram.BillogramSearch;
 import com.youcruit.billogram.objects.request.billogram.PdfFileResponse;
-import com.youcruit.billogram.objects.request.billogram.SendMethod;
 import com.youcruit.billogram.objects.response.billogram.Billogram;
+import com.youcruit.billogram.objects.response.billogram.SearchBillogram;
 import com.youcruit.billogram.objects.response.billogram.Unit;
+import com.youcruit.billogram.objects.response.common.BillogramState;
 import com.youcruit.billogram.objects.response.customer.BillogramCustomer;
 import com.youcruit.billogram.objects.response.customer.Customer;
 import com.youcruit.billogram.objects.response.item.BillogramItem;
@@ -49,7 +60,7 @@ public class BillogramIT extends HttpIT {
 	billogram2.withCurrency("SEK");
 	final Billogram billogram3 = billogramClient.update(billogram2).getData();
 
-	billogramClient.command(billogram3.getId(), SendMethod.EMAIL, BillogramClient.SendCommand.SEND);
+	billogramClient.command(billogram3.getId(), EMAIL, SEND);
 	PdfFileResponse pdf = null;
 	int attemptsLeft = 5;
 	while(true) {
@@ -68,7 +79,6 @@ public class BillogramIT extends HttpIT {
 	assertTrue(pdf.getData().asByteArray().length != 0);
     }
 
-
     @Test
     public void createRetrieveAndSell() throws IOException, InterruptedException {
 	CustomerClient customerClient = new CustomerClient(client);
@@ -84,5 +94,32 @@ public class BillogramIT extends HttpIT {
 	final Billogram billogram3 = billogramClient.update(billogram2).getData();
 
 	billogramClient.command(billogram3.getId(), BillogramClient.Command.SELL);
+    }
+
+    @Test
+    public void testSearch() throws IOException, InterruptedException {
+	CustomerClient customerClient = new CustomerClient(client);
+	Customer customer = CustomerHelper.createCustomer(customerClient);
+
+	final Billogram billogram = new Billogram().withCustomer(new BillogramCustomer().withCustomerNo(customer.getCustomerNo()));
+	final BillogramItem item = new BillogramItem().withCount(1).withDescription("foodesc").withDiscount(BigDecimal.valueOf(5)).withItemNo("34567");
+	item.withPrice(new BigDecimal("12345.25")).withTitle("titledesc").withUnit(Unit.UNIT).withVat(6);
+
+	billogram.getItems().add(item);
+	final Billogram billogram2 = billogramClient.create(billogram).getData();
+	billogram2.withCurrency("SEK");
+	final Billogram billogram3 = billogramClient.update(billogram2).getData();
+
+	//billogramClient.command(billogram3.getId(), EMAIL, SEND);
+
+	Search<BillogramFilterField, BillogramOrderField> search = new BillogramSearch();
+	search.withFilter(FilterType.FIELD, BillogramFilterField.STATE, BillogramState.UNATTESTED.toString());
+	search.withOrder(SortDirection.DESCENDING, CUSTOMER_NO);
+	for(SearchBillogram result : billogramClient.search(search).getData()) {
+	    if (result.getId().equals(billogram3.getId())) {
+		return;
+	    }
+	}
+	fail("Didn not find the document " + billogram3.getId());
     }
 }
